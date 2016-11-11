c299815993e1c559ebd73b152bb11f34ae4c3265
geoffberl <gberl001@gmail.com>
2016-11-11 13:16:36 -0500

    Initial commit, INTERRUPT_MODE is non-functional

---
+++ TSL235R.cpp
@@ -0,0 +1,92 @@
+
+#include <Arduino.h>
+#include "TSL235R.h"
+
+TSL235R::TSL235R(unsigned char dataPin) {
+    // Set the default mode to use interrupt (better accuracy)
+    init(dataPin, INTERRUPT_MODE);
+}
+
+TSL235R::TSL235R(unsigned char dataPin, char mode) {
+    init(dataPin, mode);
+}
+
+void TSL235R::init(unsigned char dataPin, char mode) {
+    // Set up the data pin
+    TSL235R::dataPin = dataPin;
+    pinMode(dataPin, INPUT);
+    digitalWrite(dataPin, HIGH);
+
+    // Set the desired mode
+    TSL235R::mode = mode;
+
+    // Default min and max and other variables
+    min = 2000000;
+    max = 0;
+    lastTime = micros();
+    sampleRateUS = 1000;
+    interruptDuration = 0.0;
+}
+
+void TSL235R::handleInterrupt() {
+    cli();
+    unsigned long currentTime = micros();
+    counter++;
+
+    // Check if the sample rate has elapsed
+    if ((currentTime - lastTime) >= sampleRateUS) {
+        interruptDuration = sampleRateUS / counter;
+        counter = 0;
+        lastTime = currentTime;
+    }
+
+    sei();
+}
+
+unsigned long TSL235R::read() {
+    unsigned long returnValue = 0;
+    switch (mode) {
+        case PULSE_MODE: {
+            unsigned long pulseDuration = pulseIn(dataPin, HIGH);
+
+            // Convert the result to frequency (# us in 1 second / pulse-width in us * 2)
+            // Multiply the value by two since we only calculated the pulse width of half a cycle
+            returnValue = 1000000 / (pulseDuration * 2);
+            break;
+        }
+        case INTERRUPT_MODE: {
+            Serial.println(interruptDuration);
+            // Convert the result to frequency, here we are actually calculating a full cycle but the sample rate
+            returnValue = (unsigned long) (1000000 / interruptDuration);
+            break;
+        }
+        default:
+            returnValue = 0;
+    }
+    return returnValue;
+}
+
+char TSL235R::readCalibrated() {
+    if (max ==0) {
+        return 0;
+    }
+
+    // Return a normalized value between 0 and 100
+    return (char) ((((float)read()) - min) / range * 100);
+}
+
+void TSL235R::calibrateOnce() {
+    // Get a current reading
+    unsigned long currentValue = read();
+
+    // Adjust min and max
+    if (currentValue > max) {
+        max = currentValue;
+    }
+    if (currentValue < min) {
+        min = currentValue;
+    }
+
+    // Adjust the range, we store this to save on calculating it later
+    range = max - min;
+}
---
+++ TSL235R.h
@@ -0,0 +1,62 @@
+#ifndef TSL235R_H
+#define TSL235R_H
+
+#define INTERRUPT_MODE 0
+#define PULSE_MODE 1
+
+
+class TSL235R {
+public:
+    TSL235R(unsigned char dataPin);
+
+    TSL235R(unsigned char dataPin, char mode);
+
+    /**
+     * handleInterrupt
+     * If you are using the interrupt method you must set up an interrupt
+     * in the main program and call this method each time the interrupt
+     * service routine is triggered.
+     */
+    void handleInterrupt();
+
+    /**
+     * read
+     * Takes an instant reading the light intensity. In actuality it takes
+     * multiple readings and averages them to avoid spurios noise effects.
+     * @return int the frequency (Hz) of the sensor signal
+     */
+    unsigned long read();
+
+    /**
+     * readCalibrated
+     * Takes an instant reading and normalizes the value utilizing
+     * the calibrated min and max values. The ultimate value returned is a value
+     * between 0 and 1000.
+     * @return char the intensity value between 0 and 100
+     */
+    char readCalibrated();
+
+    /**
+     * calibrateOnce
+     * Takes exactly one reading and adjusts the min and max values. Ideally
+     * this method would be called repeatedly a certain number of times or for
+     * a certain amount of elapsed time.
+     */
+    void calibrateOnce();
+
+private:
+    void init(unsigned char dataPin, char mode);
+
+    char mode;
+    unsigned char dataPin;
+    volatile unsigned long counter;
+    unsigned long max;
+    unsigned long min;
+    unsigned long range;
+    volatile unsigned long lastTime;
+    unsigned int sampleRateUS;
+    volatile float interruptDuration;
+
+};
+
+#endif // TSL235R_H
